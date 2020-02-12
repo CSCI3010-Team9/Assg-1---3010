8cb95b96eb2be2284e7a9d8323ea792b3d8c1abd
smithjere14 <smithjere14@students.ecu.edu>
2020-02-12 11:34:49 -0500

    Added the Client and Server Project files

---
+++ ActivationServer/ActivationServer/Server.cpp
@@ -0,0 +1,51 @@
+#include <WinSock2.h>
+#include <Ws2tcpip.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+// Make sure build environment links to Winsock library file
+#pragma comment(lib, "Ws2_32.lib")
+
+
+// Define default global constants
+#define BUFFERSIZE 256
+#define DATAFILENAME "dataFile.txt"
+#define IPADDRESS "127.0.0.1"
+#define GOODMSG "good"
+#define BADMSG "invalid"
+#define DEFAULTPORT 6000
+
+
+// Function to close the specified socket and perform DLL cleanup (WSACleanup)
+void cleanup(SOCKET socket);
+
+
+int main(int argc, char* argv[])
+{
+	int port;
+
+	// If user types in a port number on the command line use it,
+	// otherwise, use the default port number
+	if (argc > 1)
+		port = atoi(argv[1]);
+	else
+		port = DEFAULTPORT;
+
+
+	
+
+	return 0;
+}
+
+
+void cleanup(SOCKET socket)
+{
+	if (socket != INVALID_SOCKET)
+		closesocket(socket);
+
+	WSACleanup();
+}
+
+
