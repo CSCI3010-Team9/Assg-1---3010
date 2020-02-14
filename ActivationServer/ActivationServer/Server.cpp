646582fdfafaf8c51ed5d5581632ed795af6ed0d
smithjere14 <smithjere14@students.ecu.edu>
2020-02-14 09:28:20 -0500

    added chatLog function started adding code from ChatServer Server example

--- ActivationServer/ActivationServer/Server.cpp
+++ ActivationServer/ActivationServer/Server.cpp
@@ -21,17 +21,48 @@ using namespace std;
 // Function to close the specified socket and perform DLL cleanup (WSACleanup)
 void cleanup(SOCKET socket);
 
+void chatLog ()
+{
+    ofstream log;
+    log.open(DATAFILENAME);
+}
+
 
 int main(int argc, char* argv[])
 {
-	int port;
+    WSADATA		wsaData;			// structure to hold info about Windows
+                                    // sockets implementation
+    SOCKET		listenSocket;		// socket for listening for incoming
+                                    // connections
+    SOCKET		clientSocket;		// socket for communication with the client
+    SOCKADDR_IN	serverAddr;			// structure to hold server address
+                                    // information
+    string		ipAddress;			// server's IP address
+    string		input;				// generic input from user
+    char		buffer[BUFFERSIZE];	// buffer to hold message received from
+                                    // server
+    int			port;				// server's port number
+    int			iResult;			// resulting code from socket functions
+    bool		done;				// variable to control communication loop
+    bool		moreData;			// variable to control receive data loop
 
-	// If user types in a port number on the command line use it,
-	// otherwise, use the default port number
-	if (argc > 1)
-		port = atoi(argv[1]);
-	else
+    cout << "\n*** CHAT SERVER ***\n" << endl;
+
+    //Get server address information from user
+    cout << "Enter IP address (press enter for localhost): "
+    getline(cin, ipAddress);
+    if (ipAddress == "")
+        ipAddress = "127.0.0.1"
+
+    cout << endl;
+	// If user types in a port number on the command line use it, otherwise, use
+	// the default port number
+    cout << "Enter port number (press enter for default port): "
+    getline(cin, input)
+	if (port == "")
 		port = DEFAULTPORT;
+	else
+	    port = stoi(input);
 
 
 	
