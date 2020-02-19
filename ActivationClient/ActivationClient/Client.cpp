b7762e82260ce94219c4a9cd24656970ccd9b2a1
darkm17 <darkm17@CET-A209-12>
2020-02-19 17:13:54 +0000

    YOOOOOO

--- ActivationClient/ActivationClient/Client.cpp
+++ ActivationClient/ActivationClient/Client.cpp
@@ -1,3 +1,4 @@
+// Author:
 #include <WinSock2.h>
 #include <Ws2tcpip.h>
 #include <iostream>
@@ -24,6 +25,76 @@ void cleanup(SOCKET socket);
 
 int main(int argc, char* argv[])
 {
+	WSADATA		wsaData;			// structure to hold info about Windows sockets implementation
+	SOCKET		mySocket;			// socket for communication with the server
+	SOCKADDR_IN	serverAddr;			// structure to hold server address information
+	string		ipAddress;			// server's IP address
+	string		input;				// generic input from user
+	char		buffer[BUFFERSIZE];	// buffer to hold message received from server
+	int			port;				// server's port number
+	int			iResult;			// resulting code from socket functions
+	bool		done;				// variable to control communication loop
+	bool		moreData;			// variable to control receive data loop
+
+
+	cout << "\n*** CHAT CLIENT ***\n\n";
+
+	// Get server connection information from user
+	cout << "Enter server IP address (press enter for localhost): ";
+	getline(cin, ipAddress);
+	if (ipAddress == "")
+		ipAddress = "127.0.0.1";
+
+	cout << endl;
+	cout << "Enter server port number is " << DEFAULTPORT << endl;
+	cout << "Press enter to accept port, or enter new port: ";
+	getline(cin, input);
+	if (input == "")
+		port = DEFAULTPORT;
+	else
+		port = stoi(input);
+
+
+	// WSAStartup loads WS2_32.dll (Winsock version 2.2) used in network programming
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != NO_ERROR)
+	{
+		cerr << "WSAStartup failed with error: " << iResult << endl;
+		return 1;
+	}
+
+
+	// Create a new socket for communication with the server
+	mySocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	if (mySocket == INVALID_SOCKET)
+	{
+		cerr << "Socket failed with error: " << WSAGetLastError() << endl;
+		cleanup(mySocket);
+		return 1;
+	}
+
+	cout << "\nAttempting to connect...\n";
+
+
+	// Setup a SOCKADDR_IN structure which will be used to hold address
+	// and port information for the server. Notice that the port must be converted
+	// from host byte order to network byte order.
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(port);
+	inet_pton(AF_INET, ipAddress.c_str(), &serverAddr.sin_addr);
+
+
+	// Try to connect to server
+	iResult = connect(mySocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
+	if (iResult == SOCKET_ERROR)
+	{
+		cerr << "Connect failed with error: " << WSAGetLastError() << endl;
+		cleanup(mySocket);
+		return 1;
+	}
+
+	cout << "Connected...\n\n";
 	int port;
 
 	// If user types in a port number on the command line use it,
