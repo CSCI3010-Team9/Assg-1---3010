d7f5a65453abe16a5148422d3a4d6ca6870922b6
smithjere14 <smithjere14@students.ecu.edu>
2020-02-19 11:34:11 -0500

    changed chatLog function to not a function to be able to write to file finished adding code from ChatServer Server example

--- ActivationServer/ActivationServer/Server.cpp
+++ ActivationServer/ActivationServer/Server.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+
 using namespace std;
 
 // Make sure build environment links to Winsock library file
@@ -21,62 +22,187 @@ using namespace std;
 // Function to close the specified socket and perform DLL cleanup (WSACleanup)
 void cleanup(SOCKET socket);
 
-void chatLog ()
-{
-    ofstream log;
-    log.open(DATAFILENAME);
-}
 
+int main(int argc, char *argv[]) {
+    WSADATA wsaData;            // structure to hold info about Windows sockets implementation
+    SOCKET listenSocket;        // socket for listening for incoming connections
+    SOCKET clientSocket;        // socket for communication with the client
+    SOCKADDR_IN serverAddr;     // structure to hold server address information
+    string ipAddress;           // server's IP address
+    string input;               // generic input from user
+    char buffer[BUFFERSIZE];    // buffer to hold message received from server
+    int port;                   // server's port number
+    int iResult;                // resulting code from socket functions
+    bool done;                  // variable to control communication loop
+    bool moreData;              // variable to control receive data loop
+    ofstream MyFile;            // variable for data file
 
-int main(int argc, char* argv[])
-{
-    WSADATA		wsaData;			// structure to hold info about Windows
-                                    // sockets implementation
-    SOCKET		listenSocket;		// socket for listening for incoming
-                                    // connections
-    SOCKET		clientSocket;		// socket for communication with the client
-    SOCKADDR_IN	serverAddr;			// structure to hold server address
-                                    // information
-    string		ipAddress;			// server's IP address
-    string		input;				// generic input from user
-    char		buffer[BUFFERSIZE];	// buffer to hold message received from
-                                    // server
-    int			port;				// server's port number
-    int			iResult;			// resulting code from socket functions
-    bool		done;				// variable to control communication loop
-    bool		moreData;			// variable to control receive data loop
+    MyFile.open(DATAFILENAME, ios::out);
 
     cout << "\n*** CHAT SERVER ***\n" << endl;
 
-    //Get server address information from user
+    // Get server address information from user
     cout << "Enter IP address (press enter for localhost): "
     getline(cin, ipAddress);
     if (ipAddress == "")
-        ipAddress = "127.0.0.1"
+        ipAddress = "127.0.0.1";
 
     cout << endl;
-	// If user types in a port number on the command line use it, otherwise, use
-	// the default port number
-    cout << "Enter port number (press enter for default port): "
+
+    // Get server port number from user
+    cout << "Enter port number (press enter for the default port): " << endl;
     getline(cin, input)
-	if (port == "")
-		port = DEFAULTPORT;
-	else
-	    port = stoi(input);
+    if (input == "")
+        port = DEFAULTPORT;
+    else
+        port = stoi(input);
+
+    // WSAStartup loads WS2_32.dll (Winsock version 2.2) used in network programming
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != NO_ERROR) {
+        cerr << "WSAStartup failed with error: " << iResult << endl;
+        return 1;
+    }
+
+    // Create a new socket for communication with the client
+    listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    if (listenSocket == INVALID_SOCKET) {
+        cerr << "Socket failed with error: " << WSAGetLastError() << endl;
+        cleanup(listenSocket);
+        return 1;
+    }
+
+    // Setup a SOCKADDR_IN structure which will be used to hold address and port
+    // information for the server. Notice that the port must be converted from
+    // host byte order to network byte order.
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(port);
+    inet_pton(AF_INET, ipAddress.c_str(), &serverAddr.sin_addr);
+
+    // Attempt to bind a the local network address to the socket
+    iResult = bind(listenSocket, (SOCKADDR * ) & serverAddr, sizeof(serverAddr));
+    if (iResult == SOCKET_ERROR) {
+        cerr << "Bind failed with error: " << WSAGetLastError() << endl;
+        cleanup(listenSocket);
+        return 1;
+    }
+
+    // Start listening for incoming connections
+    iResult = listen(listenSocket, 1);
+    if (iResult == SOCKET_ERROR) {
+        cerr << "Listen failed with error: " << WSAGetLastError() << endl;
+        cleanup(listenSocket);
+        return 1;
+    }
+
+    cout << "\nWaiting for connections...\n";
+
+    // Accept an incoming connection; Program pauses here until a connection arrives
+    clientSocket = accept(listenSocket, NULL, NULL);
+    if (clientSocket == INVALID_SOCKET) {
+        cerr << "Accept failed with error: " << WSAGetLastError() << endl;
+        // Need to close listenSocket; clientSocket never opened
+        cleanup(listenSocket);
+        return 1;
+    }
+
+    // For this program ONLY, once an incoming connection has been accepted, the
+    // listen socket is no longer needed so it will be closed
+    closesocket(listenSocket);
+
+    cout << "Connected...\n\n";
+
+    // Always make sure there's a '\0' at the end of the buffer
+    buffer[BUFFERSIZE - 1] = '\0';
+
+    // Loop to communicate with client until either side types "end"
+    done = false;
+    do {
+        // RECV and then SEND
+
+        // Wait to receive a message from the client
+        cout << "\n\t--WAIT--\n\n";
+
+        // String to hold received message
+        input = "";
+
+        // Receive until client stops sending data
+        moreData = false;
+        do {
+            iResult = recv(clientSocket, buffer, BUFFERSIZE - 1, 0);
+
+            if (iResult > 0) {
+                // Received data; need to determine if there's more coming
+                if (buffer[iResult - 1] != '\0')
+                    moreData = true;
+                else
+                    moreData = false;
+
+                // Concatenate received data onto end of string we're building
+                input = input + (string) buffer;
+            } else if (iResult == 0) {
+                cout << "Connection closed\n";
+                // Need to close clientSocket; listenSocket was already closed
+                cleanup(clientSocket);
+                return 0;
+            } else {
+                cerr << "Recv failed with error: " << WSAGetLastError() << endl;
+                cleanup(clientSocket);
+                return 1;
+            }
+
+        } while (moreData);
+
+        if (input == "1234")
+            cout << "Serial Number: " << input << GOODMSG << endl;
+            MyFile
+        else
+            cout << "Serial Number: " << input << BADMSG << endl;
+
+        if (input == "abcd")
+            cout << "Machine ID: " << input << GOODMSG << endl;
+        else
+            cout << "Machine ID: " << input << BADMSG << endl;
+
+        cout << "Recv > " << input << endl;
 
 
-	
+        // Communication ends if client sent an "end" message
+        if (input == "end")
+            done = true;
 
-	return 0;
+
+        if (!done) {
+            // Get a message from the user and attempt to send it to the client
+            cout << "Send > ";
+            getline(cin, input);
+
+            iResult = send(clientSocket, input.c_str(), (int) input.size() + 1, 0);
+            if (iResult == SOCKET_ERROR) {
+                cerr << "Send failed with error: " << WSAGetLastError() << endl;
+                cleanup(clientSocket);
+                return 1;
+            }
+
+            // Communication ends if server sent an "end" message
+            if (input == "end")
+                done = true;
+        }
+
+    } while (!done);
+
+    // Remember, need to close clientSocket before exiting, listenSocket was previously closed
+    cleanup(clientSocket);
+    return 0;
+}
+
 }
 
 
-void cleanup(SOCKET socket)
-{
-	if (socket != INVALID_SOCKET)
-		closesocket(socket);
+void cleanup(SOCKET socket) {
+    if (socket != INVALID_SOCKET)
+        closesocket(socket);
 
-	WSACleanup();
+    WSACleanup();
 }
-
-
