cc18f46011b9e55e51031e44f2e47bfab9b86033
quinton jones <kjdatruf@hotmail.com>
2020-02-19 23:15:43 -0500

    mr.darks changes

--- ActivationClient/ActivationClient/Client.cpp
+++ ActivationClient/ActivationClient/Client.cpp
@@ -104,6 +104,106 @@ int main(int argc, char* argv[])
 	else
 		port = DEFAULTPORT; 
 	
+	// Always make sure there's a '\0' at the end of the buffer
+
+	buffer[BUFFERSIZE - 1] = '\0';
+
+	// Loop to communicate with server until either side types "end"
+
+	done = false;
+
+	do
+	{
+		// SEND and then RECV
+
+		// Get a message from the user and attempt to send it to the server
+
+		cout << "Send serial number> ";
+
+		getline(cin, input);
+
+		iResult = send(mySocket, input.c_str(), (int)input.size() + 1, 0);
+
+		if (iResult == SOCKET_ERROR)
+		{
+			cerr << "Serial send failed with error: " << WSAGetLastError() << endl;
+
+			cleanup(mySocket);
+
+			return 1;
+		}
+
+		// Communication ends if client sent an "end" message
+
+		if (input == "end")
+
+			done = true;
+
+		if (!done)
+		{
+			// Wait to receive a reply message back from the server
+
+			cout << "\n\t--WAIT--\n\n";
+
+			// String to hold received message
+
+			input = "";
+
+			// Receive until server stops sending data
+
+			moreData = false;
+
+			do
+			{
+				iResult = recv(mySocket, buffer, BUFFERSIZE - 1, 0);
+
+				if (iResult > 0)
+				{
+					// Received data; need to determine if there's more coming                                                        
+
+					if (buffer[iResult - 1] != '\0')
+						moreData = true;
+					else
+						moreData = false;
+
+					// Concatenate received data onto end of string we're building
+
+					input = input + (string)buffer;
+
+				}
+
+				else if (iResult == 0)
+				{
+					cout << "Connection closed\n";
+
+					cleanup(mySocket);
+
+					return 0;
+				}
+
+				else
+				{
+					cerr << "Recv failed with error: " << WSAGetLastError() << endl;
+
+					cleanup(mySocket);
+
+					return 1;
+				}
+
+			} while (moreData);
+
+			cout << "Recv > " << input << endl;
+
+			// Communication ends if server sent an "end" message
+
+			if (input == "end")
+
+				done = true;
+		}
+
+
+
+	} while (!done);
 	
 
 	return 0;
